fc56db637c69bc036231760b56bcb4549558ec46
kunal777 <gagarekamal777@gmail.com>
2025-04-06 21:56:41 -0700

    worked on db config

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,19 +3,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  // database: { 
-  //   host: "localhost",
-  //   user: "root",
-  //   password: "Kunal@777",
-  //   database: "mySql_db",
-  //   port : 3306
-  // },
-
   database: { 
     host: "sql12.freesqldatabase.com",
     user: "sql12771604",
     password: "cS4D3lM67k",
     database: "sql12771604",
     port : 3306
-  },
+  }
 };
+
+// database: { 
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "Kunal@777",
+  //   database: "mySql_db",
+  //   port : 3306
+  // },
